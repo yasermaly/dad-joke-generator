614d3532f67379db6456cdac50efcae0b04e82d6
Yaser Aly <yaser-aly@live.com>
2023-09-22 16:36:41 -0700

    dad joke generator complete

--- script.js
+++ script.js
@@ -13,15 +13,22 @@ const options = {
 const apiURL = 'https://api.api-ninjas.com/v1/dadjokes?limit=1';
 
 async function getJoke() {
-  joke.innerText = 'Updating..';
-  btn.disabled = true;
-  btn.innerText = 'Loading..';
-  const response = await fetch(apiURL, options);
-  const data = await response.json();
+  try {
+    joke.innerText = 'Updating..';
+    btn.disabled = true;
+    btn.innerText = 'Loading..';
+    const response = await fetch(apiURL, options);
+    const data = await response.json();
 
-  btn.disabled = false;
-  btn.innerText = 'Tell me a joke';
-  joke.innerText = data[0].joke;
+    btn.disabled = false;
+    btn.innerText = 'Tell me a joke';
+    joke.innerText = data[0].joke;
+  } catch (error) {
+    btn.disabled = false;
+    btn.innerText = 'Tell me a joke';
+    joke.innerText = 'An error happened, please try again later';
+    console.log(error);
+  }
 }
 
 btn.addEventListener('click', getJoke);
