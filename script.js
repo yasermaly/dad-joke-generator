d563a6217521752d2afb85a72e0b6513bb800b2e
Yaser Aly <yaser-aly@live.com>
2023-09-21 21:02:30 -0700

    fetching response from dad jokes api and prining response object to the console

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('script file linked');
+const btn = document.getElementById('btn');
+const apiKey = 'FWqcQt29AwxHLF20+Hy+KQ==Bfeqio6zeRQpNEsP';
+
+const options = {
+  method: 'GET',
+  headers: {
+    'X-Api-Key': apiKey,
+  },
+};
+
+const apiURL = 'https://api.api-ninjas.com/v1/dadjokes?limit=1';
+
+async function getJoke() {
+  const response = await fetch(apiURL, options);
+  const data = await response.json();
+
+  console.log(data);
+}
+
+btn.addEventListener('click', getJoke);
