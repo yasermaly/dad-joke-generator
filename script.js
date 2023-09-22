94c780d80148e562b039e8d732ede70e19e6d70f
Yaser Aly <yaser-aly@live.com>
2023-09-21 21:13:06 -0700

    displaying fetched joke in the UI

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const btn = document.getElementById('btn');
+const joke = document.getElementById('joke');
+
 const apiKey = 'FWqcQt29AwxHLF20+Hy+KQ==Bfeqio6zeRQpNEsP';
 
 const options = {
@@ -11,10 +13,15 @@ const options = {
 const apiURL = 'https://api.api-ninjas.com/v1/dadjokes?limit=1';
 
 async function getJoke() {
+  joke.innerText = 'Updating..';
+  btn.disabled = true;
+  btn.innerText = 'Loading..';
   const response = await fetch(apiURL, options);
   const data = await response.json();
 
-  console.log(data);
+  btn.disabled = false;
+  btn.innerText = 'Tell me a joke';
+  joke.innerText = data[0].joke;
 }
 
 btn.addEventListener('click', getJoke);
